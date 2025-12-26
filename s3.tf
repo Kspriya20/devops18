ed2329a59f04ead62cd1387c90529c5ac1fa9fd4
Kspriya20 <96160717+Kspriya20@users.noreply.github.com>
2025-12-26 16:39:54 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -22,3 +22,12 @@ versioning_configuration {
 status = "Enabled"
 }
 }
+
+terraform {
+backend "s3" {
+region = "us-east-1"
+bucket = "sonyypriya1122.monobucket"
+key = "prod/terraform.tfstate"
+}
+}
+
